d8546152860392a2b9e8a01d9da0098053ea9afa
Andriy Kogut <kogut.andriy@gmail.com>
2016-05-01 12:31:18 +0300

    better email template

--- reddit_mailer/digester/digester.py
+++ reddit_mailer/digester/digester.py
@@ -13,9 +13,13 @@ class Digester(object):
     def digest(self, subreddit, limit, to):
         r = praw.Reddit(user_agent=USER_AGENT)
         submissions = r.get_subreddit(subreddit).get_top(limit=limit)
-        text = "\n".join([str(s) for s in submissions])
-        subject = "Top {} from /r/{}".format(limit, subreddit)
-        self.mailer.send_digest(
-            {"text": text, "subject": subject},
-            to
-        )
+        context = {
+            "r": subreddit,
+            "limit": limit,
+            "posts": [{
+                "score": s.score,
+                "permalink": s.permalink,
+                "title": s.title
+            } for s in submissions]
+        }
+        self.mailer.send_digest(context, to)
--- reddit_mailer/mailer/mailer.py
+++ reddit_mailer/mailer/mailer.py
@@ -4,15 +4,23 @@ import smtplib
 from email.mime.text import MIMEText
 
 from nameko.rpc import rpc
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+TEMPLATE = Environment(
+    loader=FileSystemLoader('./templates/')
+).get_template('digest.jinja2')
 
 
 class Mailer(object):
     name = "mailer"
 
     @rpc
-    def send_digest(self, digest, to):
-        msg = MIMEText(digest['text'])
-        msg['Subject'] = digest['subject']
+    def send_digest(self, context, to):
+        message = TEMPLATE.render(context)
+        msg = MIMEText(message, 'html')
+        msg['Subject'] = "Top {} from /r/{}".format(
+            context['limit'], context['r'])
         msg['From'] = os.getenv('DIGEST_FROM', 'andriy.kogut@gmail.com')
         msg['To'] = to
         smtp = smtplib.SMTP(
